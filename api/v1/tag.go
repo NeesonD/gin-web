9d62d6fe284d011ac63d83701b13e47293be7c0b
neeson <d1043358774@gmail.com>
2019-12-29 19:17:22 +0800

    tag service

---
+++ api/v1/tag.go
@@ -0,0 +1,74 @@
+package v1
+
+import (
+	"gin-web/models"
+	"gin-web/pkg/e"
+	"gin-web/pkg/setting"
+	"gin-web/pkg/util"
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+	"net/http"
+)
+
+func GetTags(ctx *gin.Context) {
+	name := ctx.Query("name")
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+
+	if name != "" {
+		maps["name"] = name
+	}
+
+	var state = -1
+	if arg := ctx.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] = state
+	}
+
+	code := e.SUCCESS
+
+	data["list"] = models.GetTags(util.GetPage(ctx), setting.PageSize, maps)
+	data["total"] = models.GetTagTotal(maps)
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
+}
+
+func AddTag(ctx *gin.Context) {
+	name := ctx.Query("name")
+	state := com.StrTo(ctx.DefaultQuery("state", "0")).MustInt()
+	createdBy := ctx.Query("created_by")
+
+	valid := validation.Validation{}
+	valid.Required(name, "name").Message("名称不能为空")
+	valid.MaxSize(name, 100, "name").Message("名称最长100个字符")
+	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if !models.ExistTagByName(name) {
+			code = e.SUCCESS
+			models.AddTag(name, state, createdBy)
+		} else {
+			code = e.ERROR_EXIST_TAG
+		}
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
+}
+
+func EditTag(ctx *gin.Context) {
+
+}
+
+func DeleteTag(ctx *gin.Context) {
+
+}
--- routers/route.go
+++ routers/route.go
@@ -1,21 +1,28 @@
 package routers
 
 import (
+	v1 "gin-web/api/v1"
 	"gin-web/pkg/setting"
 	"github.com/gin-gonic/gin"
 )
 
-func InitRouter() *gin.Engine  {
+func InitRouter() *gin.Engine {
 	r := gin.New()
 
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
 	gin.SetMode(setting.RunMode)
-	r.GET("/test", func(context *gin.Context) {
-		context.JSON(200,gin.H{
-			"message": "hello",
-		})
-	})
+
+	apiV1 := r.Group("api/v1")
+
+	{
+		apiV1.GET("/tags", v1.GetTags)
+		apiV1.POST("/tags", v1.AddTag)
+		apiV1.PUT("/tags/:id", v1.EditTag)
+		apiV1.DELETE("/tags/:id", v1.DeleteTag)
+
+	}
+
 	return r
 }
